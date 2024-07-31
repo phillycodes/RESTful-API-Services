24539a5cebfd5539380ef0a546bcd2ace44e43c7
phillycodes <pierre22@duck.com>
2024-07-31 17:08:02 -0500

    added comments

--- src/main/java/com/restapi/mysql/MysqlApplication.java
+++ src/main/java/com/restapi/mysql/MysqlApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+/* Author: Philben Pierre
+* Date: 07/31/2024
+* Description: Springboot projected created connected to connect a Web UI (Thymeleaf) to a MySQL database.
+* The Application can perform CRUD operations and store items within a cache. Springboot annotation to highlight: @CacheEvict, @Caching.
+ */
 
 @SpringBootApplication
 @EnableCaching // Must be added to enable the use of caching in the application.
